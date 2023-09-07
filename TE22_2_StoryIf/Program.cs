a236dc53ec587fa57ceeae74d027d543bd614e4a
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-07 10:09:31 +0200

    Initial commit

---
+++ TE22_2_StoryIf/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Skriv in ditt namn:");
+string name = Console.ReadLine().ToLower();
+
+// name = name.ToLower();
+
+if (name == "micke")
+{
+  Console.WriteLine("Välkommen!");
+}
+else if (name == "martin")
+{
+  Console.WriteLine("Hej hej");
+}
+else
+{
+  Console.WriteLine("Fuck off!");
+}
+
+// if (name != "micke")
+// {
+// }
+
+
+Console.ReadLine();
